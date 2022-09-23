f9a62a996cc12db31ad6d256449405d1dfa72072
Aabzyx <77335648+Aabzyx@users.noreply.github.com>
2022-09-23 15:33:34 +0200

    Réparation + noms
    Réparé le code + noms dans l'index.html

--- super_duper_frontend/assets/js/sudoku.js
+++ super_duper_frontend/assets/js/sudoku.js
@@ -171,7 +171,18 @@ function tableToArray(table) {
 }
 
 async function sendToBackend() {
-  // J'ai tout cassé, hehe
-  alert("C'est cassé ! :(");
-  // ! qui à fait ça ?
+  var array = tableToArray(".sudoku-container");
+  var res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(array),
+  });
+
+  if ((await res.text()) === "true") {
+    alert("Gagné!");
+  } else {
+    alert("Perdu!");
+  }
 }
